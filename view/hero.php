b84556a3abd142429e175892b2cb1ad548d3416d
zeapiaji <zeapiaji@gmail.com>
2021-12-25 20:53:45 +0700

    first commit

---
+++ view/hero.php
@@ -0,0 +1,80 @@
+ <!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+  <div data-aos="fade-up" data-aos-delay="100">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="mahput.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <a href="#about" class="btn-get-started scrollto">Lihat</a>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="mm.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <a href="#about" class="btn-get-started scrollto">Lihat</a>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="rpl.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <a href="#about" class="btn-get-started scrollto">Lihat</a>
+            </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</section>
+
+ <section id="hero" class="d-flex align-items-center">
+    <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+      <div class="row icon-boxes">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-stack-line"></i></div>
+            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-palette-line"></i></div>
+            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="400">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-command-line"></i></div>
+            <h4 class="title"><a href="">Magni Dolores</a></h4>
+            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="500">
+          <div class="icon-box">
+            <div class="icon"><i class="ri-fingerprint-line"></i></div>
+            <h4 class="title"><a href="">Nemo Enim</a></h4>
+            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
---
+++ view/master.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head -->
+<?php include 'head.php' ?>
+<!-- /. Head -->
+
+<body>
+    <!-- Header -->
+    <?php include 'header.php'?>
+
+        <!-- Navbar -->
+        <?php include 'navbar.php'?>
+        <!-- /. Navbar -->
+
+    <!-- /. Header -->
+
+    <!--====================== MAIN CONTENT ======================-->
+
+        <!-- Hero -->
+        <?php include 'hero.php'?>
+        <!-- /. Hero -->
+
+        <!-- Main -->
+        <?php include 'main.php' ?>
+        <!-- /. Main -->
+
+    <!--==================== /. MAIN CONTENT =====================-->
+
+    <!-- Footer -->
+    <?php include 'footer.php' ?>
+    <!-- /.Footer -->
+
+    <!-- JS -->
+    <?php include 'js.php'?>
+    <!-- /. JS -->
+
+</body>
+</html>
